a971e7ab9a1f7ab606118035b19ce82b3e501d0e
SaiVishwas <saivishwasp@gmail.com>
2022-05-08 15:04:15 -0400

    write points to json

--- perspective_transformation/bbox_check.py
+++ perspective_transformation/bbox_check.py
@@ -63,8 +63,10 @@ def get_rotation_matrix(angle_to_rotate):
 def rotate_operation(points, angle_to_rotate, h, w):
     rotation_matrix = get_rotation_matrix(angle_to_rotate)
     # Get the size of the image after rotation (with padding)
-    big_img_w = int(abs(w * math.cos(angle_to_rotate)) + abs(h * math.sin(angle_to_rotate)))
-    big_img_h = int(abs(w * math.sin(angle_to_rotate)) + abs(h * math.cos(angle_to_rotate)))
+    big_img_w = int(abs(w * math.cos(angle_to_rotate)) + abs(
+        h * math.sin(angle_to_rotate)))
+    big_img_h = int(abs(w * math.sin(angle_to_rotate)) + abs(
+        h * math.cos(angle_to_rotate)))
 
     # The rotation on image is done using scipy.ndimage.rotate which pivots the image at the center
     # But doing the rotation using a rotation matrix pivots it at the top left corner (old_origin).
@@ -72,16 +74,14 @@ def rotate_operation(points, angle_to_rotate, h, w):
     # 2. We perform rotation using the origin (old center) as the pivot
     # 3. We reset the origin by adding (w/2, h/2) to all the points
 
-
-    points_translated = translate_operation(points, -w/2, -h/2)
+    points_translated = translate_operation(points, -w / 2, -h / 2)
 
     rotated_points = apply_matrix_operation(rotation_matrix, points_translated)
-    
-    new_points = translate_operation(rotated_points, big_img_w/2, big_img_h/2)
+
+    new_points = translate_operation(rotated_points, big_img_w / 2,
+                                     big_img_h / 2)
 
     return new_points
-    
-
 
 
 def translate_operation(points, x_t, y_t):
@@ -110,13 +110,27 @@ def get_rotated_reference(img, angle_to_rotate):
     return img_rotated
 
 
+def write_to_json(points_info, filepath):
+
+    og_json = json.load(open('bbox.json'))
+    # Writing to sample.json
+    for entry in range(len(og_json['predictions'])):
+        og_json['predictions'][entry]['x'] = points_info[entry][0]
+        og_json['predictions'][entry]['y'] = points_info[entry][1]
+
+    json_object = json.dumps(og_json, indent=4)
+    with open(filepath, "w") as outfile:
+        outfile.write(json_object)
+
+
 angle_to_rotate = math.pi / 4
 img = cv2.imread("../input_images/img1.jpg", cv2.IMREAD_UNCHANGED)
 h, w, c = img.shape
 points = get_bbox_list()
 rotated_points = rotate_operation(points, angle_to_rotate, h, w)
 # translated_points = translate_operation(rotated_points, 100, 100)
-
+write_to_json(rotated_points, "rotated_bbox.json")
 # rotated_img = cv2.imread("rotated_sample.png", cv2.IMREAD_UNCHANGED)
 rotated_img = get_rotated_reference(img, angle_to_rotate)
+
 plot_points(rotated_img, rotated_points)
